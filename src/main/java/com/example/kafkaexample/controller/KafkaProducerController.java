5334409f1cc9953c318341a5e8ee593011c988ce
Eunbin Bae <81680796+eungbean1029@users.noreply.github.com>
2024-04-11 22:26:46 +0900

    카프카 프로듀서 예제 코드 작업

--- src/main/java/com/example/kafkaexample/controller/KafkaProducerController.java
+++ src/main/java/com/example/kafkaexample/controller/KafkaProducerController.java
@@ -1,4 +1,26 @@
 package com.example.kafkaexample.controller;
 
-public class KafkaController {
+import com.example.kafkaexample.service.KafkaProducerService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/kafka")
+@RestController
+public class KafkaProducerController {
+
+    private final KafkaProducerService kafkaProducerService;
+
+    @PostMapping
+    public ResponseEntity<Void> sendMassage(
+            @RequestParam String message) {
+        this.kafkaProducerService.sendMessageToKafka(message);
+        return ResponseEntity.ok().build();
+    }
 }
--- src/main/java/com/example/kafkaexample/service/KafkaProducerService.java
+++ src/main/java/com/example/kafkaexample/service/KafkaProducerService.java
@@ -1,4 +1,25 @@
 package com.example.kafkaexample.service;
 
-public class KafkaService {
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class KafkaProducerService {
+
+    @Value("${topic.name}")
+    private String topicName;
+
+    /* Kafka Template 을 이용해 Kafka Broker 전송 */
+
+    private final KafkaTemplate<String,String> kafkaTemplate;
+
+    public void sendMessageToKafka(String message) {
+        System.out.printf("Producer Message : %s%n",message);
+        this.kafkaTemplate.send(topicName,message);
+    }
 }
